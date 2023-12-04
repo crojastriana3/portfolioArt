74f30ae2b2ab855a31c055623da1b501b23ea7f9
crojastrianDev <crojastriana3@gmail.com>
2023-12-04 18:28:04 -0500

    Added footer and start header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,33 @@
-import { Routes } from '@angular/router';
+//import { RouterModule, Routes } from '@angular/router';
 import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
 import { InitComponent } from './components/init/init.component';
+//import { NgModule, Component } from '@angular/core';
+import { AppComponent } from './app.component';
+import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: 'home', component: InitComponent },
-
-  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  {
+    path: '',
+    component: AppComponent,
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./app.component').then((m) => m.AppComponent),
+      },
+    ],
+  },
   { path: '**', component: PagenotfoundComponent },
-  { path: 'not-found', component: PagenotfoundComponent },
+
+  //{ path: 'home', component: InitComponent },
+  // { path: '', pathMatch: 'full', redirectTo: 'home' },
+  // { path: '**', component: PagenotfoundComponent },
+  //{ path: 'not-found', component: PagenotfoundComponent },
 ];
+
+/*@NgModule(
+    {
+        imports: [RouterModule.forRoot(routes,{useHash:true})],
+        exports: [RouterModule],
+    }
+)*/
--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -5,8 +5,14 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
 })
 export class FooterComponent {
+  year: number = new Date().getFullYear();
+  constructor() {}
 
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+  }
 }
