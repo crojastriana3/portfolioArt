0e39c1d5c29134d1987a747135ec059a80e6de58
crojastrianDev <crojastriana3@gmail.com>
2023-12-04 10:34:54 -0500

    Added configurations

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
+import { InitComponent } from './components/init/init.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: InitComponent },
+
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: '**', component: PagenotfoundComponent },
+  { path: 'not-found', component: PagenotfoundComponent },
+];
---
+++ src/app/models/products-details.spec.ts
@@ -0,0 +1,7 @@
+import { ProductsDetails } from './products-details';
+
+describe('ProductsDetails', () => {
+  it('should create an instance', () => {
+    expect(new ProductsDetails()).toBeTruthy();
+  });
+});
