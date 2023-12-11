b2487e965eb2bff31d870496d0976c4a00e39340
crojastrianDev <crojastriana3@gmail.com>
2023-12-11 09:57:46 -0500

    init about

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -3,12 +3,21 @@ import { HeaderComponent } from '../../shared/header/header.component';
 import { FooterComponent } from '../../shared/footer/footer.component';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { BannerComponent } from '../banner/banner.component';
+import { AboutComponent } from '../about/about.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css',
-  imports: [HeaderComponent, FooterComponent, CommonModule, RouterModule],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    CommonModule,
+    RouterModule,
+    BannerComponent,
+    AboutComponent,
+  ],
 })
 export class LayoutComponent {}
